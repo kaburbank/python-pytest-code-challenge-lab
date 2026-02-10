3cfb170be5c1377970d007e80aaf64932bedbe15
Katherine <kaburbank@gmail.com>
2026-02-10 10:48:33 -0700

    tests complete

--- test_palindrome.py
+++ test_palindrome.py
@@ -1,19 +1,26 @@
 import pytest
 from lib.palindrome import longest_palindromic_substring
 
-def test_babad():
-    result = longest_palindromic_substring("babad")
-    assert result in ("bab", "aba")
-
-def test_cbbd():
-    assert longest_palindromic_substring("cbbd") == "bb"
-
-def test_single_char():
-    assert longest_palindromic_substring("a") == "a"
-
-def test_ac():
-    result = longest_palindromic_substring("ac")
-    assert result in ("a", "c")
-
-def test_racecar():
-    assert longest_palindromic_substring("racecar") == "racecar"
+@pytest.mark.parametrize(
+    "input_str,expected",
+    [
+        ("babad", ("bab", "aba")),
+        ("cbbd", ("bb",)),
+        ("a", ("a",)),
+        ("ac", ("a", "c")),
+        ("racecar", ("racecar",)),
+        ("", ("",)),
+        ("aaaaa", ("aaaaa",)),
+        ("abcdefg", tuple("abcdefg")),
+        ("abccba", ("abccba",)),
+        ("abacdfgdcaba", ("aba", "cdc")),
+        ("1234321", ("1234321",)),
+        ("abbahello", ("abba",)),
+        ("helloracecar", ("racecar",)),
+        ("aa", ("aa",)),
+        ("ab", ("a", "b")),
+    ]
+)
+def test_longest_palindromic_substring(input_str, expected):
+    result = longest_palindromic_substring(input_str)
+    assert result in expected, f"For input '{input_str}', got '{result}', expected one of {expected}"
